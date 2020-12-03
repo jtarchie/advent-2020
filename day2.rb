e1f9b8bd8114d2e87392ba45966f7e6f19a8be62
JT Archie <jtarchie@gmail.com>
2020-12-02 20:04:03 -0700

    rubocop -A

--- day2.rb
+++ day2.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 PasswordCheck = Struct.new(:range, :letter, :password, keyword_init: true) do
   def valid?
-    [range.min-1, range.max-1].select do |index|
+    [range.min - 1, range.max - 1].select do |index|
       password[index] == letter
     end.size == 1
   end
@@ -9,13 +11,13 @@ end
 lines = File.read('day2.txt').split("\n")
 checks = lines.map do |line|
   range, letter, password = line.split(' ')
-  
+
   low, high = range.split('-').map(&:to_i)
 
   PasswordCheck.new(
     range: low..high,
     letter: letter[0],
-    password: password,
+    password: password
   )
 end
 
