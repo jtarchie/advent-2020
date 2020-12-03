3df7ed0e5423db9569c678295065661fdc2b7b16
JT Archie <jtarchie@gmail.com>
2020-12-02 20:01:55 -0700

    day 2b

--- day2.rb
+++ day2.rb
@@ -1,6 +1,8 @@
 PasswordCheck = Struct.new(:range, :letter, :password, keyword_init: true) do
   def valid?
-    range.include?(password.scan(letter).count)
+    [range.min-1, range.max-1].select do |index|
+      password[index] == letter
+    end.size == 1
   end
 end
 
@@ -9,7 +11,6 @@ checks = lines.map do |line|
   range, letter, password = line.split(' ')
   
   low, high = range.split('-').map(&:to_i)
-
 
   PasswordCheck.new(
     range: low..high,
