3d8ecb79d5547f8c01394dad3e178f90bb7a5816
JT Archie <jtarchie@gmail.com>
2020-12-05 07:58:13 -0700

    day5 part2

--- day5.rb
+++ day5.rb
@@ -14,43 +14,45 @@ end
 
 BoardingPass = Struct.new(:line) do
   def seat
-    row = 0..127
-    col = 0..7
+    @seat ||= begin
+      row = 0..127
+      col = 0..7
 
-    line[0..6].chars.each do |part|
-      case part
-      when 'F'
-        row = row.min..(row.mid)
-      when 'B'
-        row = (row.mid + 1)..row.max
-      else
-        raise "cannot handle #{part}"
+      line[0..6].chars.each do |part|
+        case part
+        when 'F'
+          row = row.min..(row.mid)
+        when 'B'
+          row = (row.mid + 1)..row.max
+        else
+          raise "cannot handle #{part}"
+        end
       end
-    end
 
-    raise "could not decode #{line} with final row #{row}" unless row.min == row.max
+      raise "could not decode #{line} with final row #{row}" unless row.min == row.max
 
-    line[7..9].chars.each do |part|
-      case part
-      when 'L'
-        col = col.min..(col.mid)
-      when 'R'
-        col = (col.mid + 1)..col.max
-      else
-        raise "cannot handle #{part}"
+      line[7..9].chars.each do |part|
+        case part
+        when 'L'
+          col = col.min..(col.mid)
+        when 'R'
+          col = (col.mid + 1)..col.max
+        else
+          raise "cannot handle #{part}"
+        end
       end
+
+      raise "could not decode #{line} with final col #{col}" unless col.min == col.max
+
+      row  = row.min
+      col  = col.min
+
+      Seat.new(
+        line,
+        row,
+        col
+      )
     end
-
-    raise "could not decode #{line} with final col #{col}" unless col.min == col.max
-
-    row  = row.min
-    col  = col.min
-
-    Seat.new(
-      line,
-      row,
-      col
-    )
   end
 end
 
@@ -69,3 +71,13 @@ max_seat_pass = boarding_passes.max_by do |pass|
 end
 
 puts max_seat_pass.seat.seat_id
+
+all_seat_ids = boarding_passes.map do |pass|
+  pass.seat.seat_id
+end
+(all_seat_ids.min..all_seat_ids.max).each do |id|
+  if !all_seat_ids.include?(id) && all_seat_ids.include?(id - 1) && all_seat_ids.include?(id + 1)
+    puts id
+    break
+  end
+end
