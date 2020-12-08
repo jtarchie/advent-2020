4fd7a9f361eea85677babbf7d761625a6d9cc13e
JT Archie <jtarchie@gmail.com>
2020-12-08 16:21:15 -0500

    refactor for structure

--- day7.rb
+++ day7.rb
@@ -2,23 +2,51 @@
 
 list_of_rules = File.readlines('day7.txt')
 
-rules = {}
-list_of_rules.each do |rule|
-  holding_color = rule.scan(/^(.*) bags contain/).flatten.first
-  containing_colors = rule.scan(/(\d+) (.*?) bags?[,.]{1}/).map do |num, color|
-    [color, num]
-  end.to_h
-
-  rules[holding_color] = containing_colors
+Rule = Struct.new(:name, :quantity) do
 end
 
-lookup = [['shiny gold']]
-containing_colors = rules.values.map(&:keys).flatten
+class Rules
+  def initialize(filename)
+    @filename = filename
+  end
 
-until containing_colors.intersection(lookup.last).empty?
-  lookup << rules.filter_map do |holding, contained|
-    holding unless contained.keys.intersection(lookup.last).empty?
+  def rules
+    @rules ||= begin
+      rules = {}
+      lines.each do |rule|
+        holding_color = rule.scan(/^(.*) bags contain/).flatten.first
+        containing_colors = rule.scan(/(\d+) (.*?) bags?[,.]{1}/).map do |quantity, color|
+          Rule.new(color, quantity)
+        end
+
+        rules[holding_color] = containing_colors
+      end
+      rules
+    end
+  end
+
+  def all_colors_contained_within_color
+    rules.values.flatten.map(&:name)
+  end
+
+  def find_colors_containing(colors)
+    rules.filter_map do |holding, contained|
+      holding unless contained.map(&:name).intersection(colors).empty?
+    end
+  end
+
+  private
+
+  def lines
+    @lines ||= File.readlines(@filename)
   end
 end
 
-puts lookup[1..-1].flatten.uniq.size
+rules = Rules.new('day7.txt')
+stack = [['shiny gold']]
+
+until rules.all_colors_contained_within_color.intersection(stack.last).empty?
+  stack << rules.find_colors_containing(stack.last)
+end
+
+puts stack[1..-1].flatten.uniq.size
